9acbbc5f662bab958173c779facc02b437637587
Myulberger <30986830+Myulberger@users.noreply.github.com>
2017-08-30 21:51:22 +0300

    task 2.2 draft 3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
-
+//update
 
 // устанавливаем часовой пояс в Московское время
 date_default_timezone_set('Europe/Moscow');
